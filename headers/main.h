c967b541c56d5ebb8f9b82f1ace63d6f5844af36
mohamed-seghaier <mohamedali.seghaier@ynov.com>
2023-03-16 09:40:19 +0100

    Initial commit

---
+++ headers/main.h
@@ -0,0 +1,18 @@
+//
+// Created by Dali on 16/03/2023.
+//
+
+#ifndef MY_SQL_MAIN_H
+#define MY_SQL_MAIN_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+
+
+int main(int, char **);
+
+
+
+#endif //MY_SQL_MAIN_H
