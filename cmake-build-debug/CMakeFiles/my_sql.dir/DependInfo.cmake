b26cdc8f1c4adf67f5b6dbc2ce747d07e1f455a0
nansmaurel <nans05@live.fr>
2023-03-17 11:10:03 +0100

    read en cours

---
+++ cmake-build-debug/CMakeFiles/my_sql.dir/DependInfo.cmake
@@ -0,0 +1,25 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/Users/nansmaurel/CLionProjects/my_sql/csv_manager.c" "CMakeFiles/my_sql.dir/csv_manager.c.o" "gcc" "CMakeFiles/my_sql.dir/csv_manager.c.o.d"
+  "/Users/nansmaurel/CLionProjects/my_sql/main.c" "CMakeFiles/my_sql.dir/main.c.o" "gcc" "CMakeFiles/my_sql.dir/main.c.o.d"
+  "/Users/nansmaurel/CLionProjects/my_sql/print/flags.c" "CMakeFiles/my_sql.dir/print/flags.c.o" "gcc" "CMakeFiles/my_sql.dir/print/flags.c.o.d"
+  "/Users/nansmaurel/CLionProjects/my_sql/print/my_printers.c" "CMakeFiles/my_sql.dir/print/my_printers.c.o" "gcc" "CMakeFiles/my_sql.dir/print/my_printers.c.o.d"
+  "/Users/nansmaurel/CLionProjects/my_sql/print/my_printf.c" "CMakeFiles/my_sql.dir/print/my_printf.c.o" "gcc" "CMakeFiles/my_sql.dir/print/my_printf.c.o.d"
+  "/Users/nansmaurel/CLionProjects/my_sql/utils/get_next_line.c" "CMakeFiles/my_sql.dir/utils/get_next_line.c.o" "gcc" "CMakeFiles/my_sql.dir/utils/get_next_line.c.o.d"
+  "/Users/nansmaurel/CLionProjects/my_sql/utils/utils.c" "CMakeFiles/my_sql.dir/utils/utils.c.o" "gcc" "CMakeFiles/my_sql.dir/utils/utils.c.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CMakeFiles/my_sql.dir/cmake_clean.cmake
@@ -0,0 +1,23 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/my_sql.dir/csv_manager.c.o"
+  "CMakeFiles/my_sql.dir/csv_manager.c.o.d"
+  "CMakeFiles/my_sql.dir/main.c.o"
+  "CMakeFiles/my_sql.dir/main.c.o.d"
+  "CMakeFiles/my_sql.dir/print/flags.c.o"
+  "CMakeFiles/my_sql.dir/print/flags.c.o.d"
+  "CMakeFiles/my_sql.dir/print/my_printers.c.o"
+  "CMakeFiles/my_sql.dir/print/my_printers.c.o.d"
+  "CMakeFiles/my_sql.dir/print/my_printf.c.o"
+  "CMakeFiles/my_sql.dir/print/my_printf.c.o.d"
+  "CMakeFiles/my_sql.dir/utils/get_next_line.c.o"
+  "CMakeFiles/my_sql.dir/utils/get_next_line.c.o.d"
+  "CMakeFiles/my_sql.dir/utils/utils.c.o"
+  "CMakeFiles/my_sql.dir/utils/utils.c.o.d"
+  "my_sql"
+  "my_sql.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/my_sql.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
